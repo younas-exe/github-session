8a0da52e3ded61f1c93a78e89d5350d64724ef56
younas-exe <younaskhan120307@gmail.com>
2025-03-03 20:36:52 +0500

    first code

---
+++ GithubSession.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GithubSession{
+
+    private static int Weight;
+
+    public static void main(String[] args) {
+         
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println(" Enter your Weight in pounds : ");
+
+    double Weight = sc.nextDouble();
+
+    System.out.println("Enter your height in inches : ");
+     
+     double Height = sc.nextDouble();
+
+     Weight  = (Weight * 0.45359237) ;
+        
+        Height =  (Height * 0.0254);
+
+        double BMI = Weight/ (Height*Height);
+
+     System.out.println(" Your BMI is : " + BMI );
+
+     
+
+
+    
+    
+    }
+
+}
