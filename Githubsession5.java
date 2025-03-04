24274ed78021f901221e918167da6e1517dc3807
younas-exe <younaskhan120307@gmail.com>
2025-03-04 11:07:14 +0500

    new code

--- Githubsession5.java
+++ Githubsession5.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Githubsession5{
+public class NewFile{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
