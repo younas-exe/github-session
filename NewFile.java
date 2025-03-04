87bc359516ffc5820167751b56f3cbced3f3a89d
younas-exe <younaskhan120307@gmail.com>
2025-03-04 11:02:32 +0500

    new code

---
+++ NewFile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Githubsession5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter power: ");
+        int y = sc.nextInt();
+        int b = (int)Math.pow(x,y); //WHY WE ARE TYPECASTING IT?
+        System.out.print(b);
+    }
+}
